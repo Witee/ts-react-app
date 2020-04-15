4bee5da6e4a68ec3414954e08c16bba7431fb1b4
Witee <xieweiting@admaster.com.cn>
2020-04-15 15:13:36 +0800

    设置路径别名, 使用 @ 符号缩写路径

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 - MOCK 数据
   使用 `http-server` `http-proxy-middleware`, 根据 `setupProxy.js` 配置 访问 `./mock` 目录的数据;
+
+- 使用 `@` 表示路径别名
+
+- 支持 `less`
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
 
-import App from "./components/App";
+import App from "@/components/App";
 
 ReactDOM.render(
   <BrowserRouter>
