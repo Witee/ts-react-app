78058b4cda07b4db6a538bf1226c159025f7e3a1
Witee <xieweiting@admaster.com.cn>
2020-04-15 15:43:08 +0800

    support css modules

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # ts 与 hooks 练习项目
 
+### 启动方法
+
+```
+npm run start
+
+npm run server
+```
+
 - MOCK 数据
   使用 `http-server` `http-proxy-middleware`, 根据 `setupProxy.js` 配置 访问 `./mock` 目录的数据;
 
 - 使用 `@` 表示路径别名
 
-- 支持 `less`
+- 支持 `CSS Models`
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,21 @@
-import React from "react";
-import { Route, Link } from "react-router-dom";
-import { Layout, Menu } from "antd";
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
 
-import Employee from "./employee";
+import Employee from './employee';
 // import Setting from "./setting";
-import "./App.css";
+
+import style from './style.module.css';
 
 const { Header, Content, Footer } = Layout;
 
 const App = ({ match }: any) => {
-  let defaultKey = match.url.replace("/", "") || "employee";
+  let defaultKey = match.url.replace('/', '') || 'employee';
 
   return (
     <Layout>
       <Header>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[defaultKey]} className="menu">
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[defaultKey]} className={style.munu}>
           <Menu.Item key="employee">
             <Link to="/employee">员工管理</Link>
           </Menu.Item>
@@ -24,15 +25,15 @@ const App = ({ match }: any) => {
         </Menu>
       </Header>
 
-      <Content className="contentWrap">
-        <div className="content">
+      <Content className={style.contentWrap}>
+        <div className={style.content}>
           <Route path="/" exact component={Employee} />
           <Route path="/employee" exact component={Employee} />
           {/* <Route path="/setting" exact component={Setting} /> */}
         </div>
       </Content>
 
-      <Footer className="footer">TypeScript in Action</Footer>
+      <Footer className={style.footer}>TypeScript in Action</Footer>
     </Layout>
   );
 };
